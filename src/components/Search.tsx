7a298d34218258339fdef28b784036ac529066e1
Kumar Piyush <kr.piyush888@gmail.com>
2023-10-31 04:08:11 +0530

    error handling and api call

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { Button, Card, Flex, Grid, Heading, TextField } from '@radix-ui/themes';
 import { MagnifyingGlassIcon, Cross1Icon } from '@radix-ui/react-icons';
 import useGeneral from '../store/general';
 import ReactJson from 'react-json-view';
 import logo from '../assets/logo.png';
 import * as ScrollArea from "@radix-ui/react-scroll-area";
+import axios from 'axios';
+import * as Toast from "@radix-ui/react-toast";
 
 const Search = () => {
     const defaultJSON: any = [
@@ -28,9 +30,58 @@ const Search = () => {
     const isMobile = useGeneral((state: any) => state.isMobile);
     const [inputValue, setInputValue] = useState('');
     const [data, setData] = useState(defaultJSON);
+    const [showError, setShowError] = useState(false);
     const handleClearInput = () => {
         setInputValue('');
     };
+
+    const ErrorToast = () => {
+        const eventDateRef = useRef(new Date());
+        const timerRef = useRef(0);
+
+        useEffect(() => {
+            return () => clearTimeout(timerRef.current);
+        }, []);
+
+        return (
+            <Toast.Provider swipeDirection="right">
+                <Toast.Root
+                    className="bg-white opacity-95 rounded-md shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] p-[15px] grid [grid-template-areas:_'title_action'_'description_action'] grid-cols-[auto_max-content] gap-x-[15px] items-center data-[state=open]:animate-slideIn data-[state=closed]:animate-hide data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=cancel]:translate-x-0 data-[swipe=cancel]:transition-[transform_200ms_ease-out] data-[swipe=end]:animate-swipeOut"
+                    open={showError}
+                    onOpenChange={setShowError}
+                >
+                    <Toast.Title className="[grid-area:_title] mb-[5px] font-medium text-slate12 text-[15px]">
+                        Parse Error
+                    </Toast.Title>
+                    <Toast.Description asChild>
+                        <time
+                            className="[grid-area:_description] m-0 text-slate11 text-[13px] leading-[1.3]"
+                            dateTime={eventDateRef.current.toISOString()}
+                        >
+                           An error occured while parsing the sentence. Please try again.
+                        </time>
+                    </Toast.Description>
+                    <Toast.Action className="[grid-area:_action]" asChild altText="Goto schedule to undo">
+                        <button className="inline-flex items-center justify-center rounded font-medium text-xs px-[10px] leading-[25px] h-[25px] bg-green2 text-green11 shadow-[inset_0_0_0_1px] shadow-green7 hover:shadow-[inset_0_0_0_1px] hover:shadow-green8 focus:shadow-[0_0_0_2px] focus:shadow-green8">
+                            OK
+                        </button>
+                    </Toast.Action>
+                </Toast.Root>
+                <Toast.Viewport className="[--viewport-padding:_25px] fixed bottom-0 right-0 flex flex-col p-[var(--viewport-padding)] gap-[10px] w-[390px] max-w-[100vw] m-0 list-none z-[2147483647] outline-none" />
+            </Toast.Provider>
+        );
+    };
+
+    async function handleSend() {
+        axios.post('https://api.geolingo.vocarista.com/parse', {
+            sentence: inputValue
+        }).then((response) => {
+            setData(response.data);
+        }).catch((error) => {
+            console.log(error);
+            setShowError(true);
+        })
+    }
 
     return (
         <Grid columns={`${isMobile ? `1` : `2`} auto`} width={`100%`} align="center">
@@ -54,7 +105,7 @@ const Search = () => {
                             </Button>
                         </TextField.Slot>
                     </TextField.Root>
-                    <Button size="4">Search</Button>
+                    <Button size="4" onClick={handleSend}>Search</Button>
                 </Flex>
                 <Card
                     variant="classic"
@@ -82,6 +133,7 @@ const Search = () => {
                         </ScrollArea.Root>
                     </Flex>
                 </Card>
+                <ErrorToast />
             </Flex>
         </Grid>
     );
